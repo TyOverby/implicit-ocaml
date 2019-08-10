0c19df2b0eecdeb19a7adc980e524565bd0f7ab7
Ty Overby <ty@pre-alpha.com>
2019-08-10 21:19:47 +0000

    _

--- jitsy.ml
+++ jitsy.ml
@@ -1,16 +1,15 @@
-module Expr : sig
-  type 'a t
+open Core_kernel
+open Async
 
-  val var : 'a Ctypes.typ -> 'a t
-  val int_lit : int -> int t
-  val bool_lit : bool -> bool t
-  val add_int : int t -> int t -> int t
-  val eq_int : int t -> int t -> bool t
-  val cond : bool t -> 'a t -> 'a t -> 'a t
-  val typeof : 'a t -> 'a Ctypes.typ
-end = struct
+module Id = struct
+  include Core_kernel.Unique_id.Int ()
+
+  let to_string t = "var_" ^ to_string t
+end
+
+module Expr = struct
   type 'a t =
-    | Var : 'a Ctypes.typ -> 'a t
+    | Var : 'a Ctypes.typ * Id.t -> 'a t
     | Int_lit : int -> int t
     | Bool_lit : bool -> bool t
     | Add_int : int t * int t -> int t
@@ -18,7 +17,7 @@ end = struct
     | Cond : 'a Ctypes.typ * bool t * 'a t * 'a t -> 'a t
 
   let rec typeof (type a) : a t -> a Ctypes.typ = function
-    | Var typ -> typ
+    | Var (typ, _) -> typ
     | Int_lit _ -> Ctypes.int
     | Bool_lit _ -> Ctypes.bool
     | Add_int _ -> Ctypes.int
@@ -29,7 +28,6 @@ end = struct
   and bool_lit i = Bool_lit i
   and add_int a b = Add_int (a, b)
   and eq_int a b = Eq_int (a, b)
-  and var typ = Var typ
 
   and cond c t f =
     let typ = typeof t in
@@ -37,76 +35,171 @@ end = struct
   ;;
 end
 
-module Function : sig
-  val with_parameter
-    :  'a Ctypes_static.typ
-    -> f:('a Expr.t -> 'b Expr.t * 'c Ctypes_static.fn)
-    -> 'b Expr.t * ('a -> 'c) Ctypes_static.fn
+module Type = struct
+  type 'a t = 'a Ctypes.typ
 
-  val constant : 'a Expr.t -> 'a Expr.t * 'a Ctypes_static.fn
-end = struct
-  let with_parameter typ ~f =
-    let ( @-> ) = Ctypes.( @-> ) in
-    let expr, expr_typ = f (Expr.var typ) in
-    expr, typ @-> expr_typ
-  ;;
-
-  let constant expr =
-    let typ = Expr.typeof expr in
-    expr, Ctypes.returning typ
-  ;;
+  let to_string a = Ctypes.string_of_typ a
 end
 
-module Function_builder : sig
-  type ('a, 'r) t
-  type 'a param
-  type 'a param_type
-
-  module Let_syntax : sig
-    val return : 'a Expr.t -> ('a, 'a) t
-
-    module Let_syntax : sig
-      module Open_on_rhs : sig
-        val arg : 'a param_type -> 'a param
-        val bool : bool param_type
-        val int : int param_type
-      end
-
-      val bind : 'a param -> f:('a Expr.t -> ('b, 'c) t) -> ('b, 'a -> 'c) t
-    end
-  end
-end = struct
-  type ('a, 'r) t = 'a Expr.t * 'r Ctypes.fn
-
-  type 'a param_type =
-    { typ : 'a Ctypes.typ
-    ; c_name : string
+module Function = struct
+  type ('a, 'r) t =
+    { expression : 'a Expr.t
+    ; typ : 'r Ctypes.fn
+    ; param_map : (Id.t * string) list
     }
 
   type 'a param =
-    { typ : 'a param_type
-    ; id : unit
+    { typ : 'a Type.t
+    ; id : Id.t
     }
 
+  let with_parameter { typ; id } ~f =
+    let ( @-> ) = Ctypes.( @-> ) in
+    let { expression; typ = expr_typ; param_map } = f (Expr.Var (typ, id)) in
+    let param_map = (id, Type.to_string typ) :: param_map in
+    { expression; typ = typ @-> expr_typ; param_map }
+  ;;
+
   module Let_syntax = struct
-    let return = Function.constant
+    let return constant =
+      { expression = constant
+      ; typ = Ctypes.returning (Expr.typeof constant)
+      ; param_map = []
+      }
+    ;;
 
     module Let_syntax = struct
-      module Open_on_rhs = struct
-        let arg typ = { typ; id = () }
-        let bool = { typ = Ctypes.bool; c_name = "bool" }
-        let int = { typ = Ctypes.int; c_name = "int" }
-      end
-
-      let bind param ~f = Function.with_parameter param.typ.typ ~f
+      let bind param ~f =
+        let arg = { typ = param; id = Id.create () } in
+        with_parameter arg ~f
+      ;;
     end
   end
 end
 
-open Function_builder.Let_syntax
+module Compile = struct
+  let rec compile_expression : type a. Buffer.t -> a Expr.t -> string =
+   fun buffer expr ->
+    let open Expr in
+    let temp = Id.to_string (Id.create ()) in
+    let typ = Type.to_string (Expr.typeof expr) in
+    let rprint s =
+      bprintf buffer "%s %s = " typ temp;
+      bprintf buffer (s ^^ ";\n")
+    in
+    (match expr with
+    | Int_lit i -> rprint "%d" i
+    | Bool_lit b -> rprint "%b" b
+    | Add_int (a, b) ->
+      let temp_a = compile_expression buffer a in
+      let temp_b = compile_expression buffer b in
+      rprint "%s + %s" temp_a temp_b
+    | Eq_int (a, b) ->
+      let temp_a = compile_expression buffer a in
+      let temp_b = compile_expression buffer b in
+      rprint "%s == %s" temp_a temp_b
+    | Cond (_, c, a, b) ->
+      let temp_c = compile_expression buffer c in
+      let temp_a = compile_expression buffer a in
+      let temp_b = compile_expression buffer b in
+      rprint "%s ? %s : %s" temp_c temp_a temp_b
+    | Var (_, id) -> rprint "%s" (Id.to_string id));
+    temp
+ ;;
+
+  let compile ~name { Function.expression; typ = _; param_map } =
+    let buffer = Buffer.create 32 in
+    bprintf buffer "extern %s %s(" (Type.to_string (Expr.typeof expression)) name;
+    param_map
+    |> List.map ~f:(fun (id, typ) -> sprintf "%s %s" typ (Id.to_string id))
+    |> List.intersperse ~sep:", "
+    |> List.iter ~f:(Buffer.add_string buffer);
+    bprintf buffer ") {\n";
+    let return_temp = compile_expression buffer expression in
+    bprintf buffer "return %s;\n" return_temp;
+    bprintf buffer "}";
+    Buffer.contents buffer
+  ;;
+
+  let compile_c source = 
+   let open Async.Deferred.Or_error.Let_syntax in 
+   let name, writer = Core.Filename.open_temp_file "prefix" ".c" in
+   Out_channel.output_string writer source;
+   Out_channel.flush writer ;
+   Out_channel.close writer;
+   let basepath, _  = Core.Filename.split_extension name in
+   let out = basepath ^ ".so" in 
+   let log = basepath ^ ".sh" in 
+   let args = ["-shared"; name; "-lm"; "-o"; out] in
+   Out_channel.write_all log ~data:(String.concat ("gcc"::args) ~sep:" ");
+   let%bind _: string  = Async.Process.run () ~prog:"gcc" ~args in 
+   return out
+
+ let load source = 
+  Dl.dlopen ~filename:source ~flags:[Dl.RTLD_NOW] 
+
+ let jit f = 
+  let open Async.Deferred.Let_syntax in 
+  let name = Id.create () |> Id.to_string in 
+  let c_source = compile ~name f in
+  let%bind compiled_filename = compile_c c_source |> Deferred.Or_error.ok_exn  in 
+  let library =  load compiled_filename in 
+  return (Foreign.foreign ~from:library name  f.typ)
+end
 
 let f =
-  let%bind_open a = arg int in
-  let%bind_open b = arg int in
-  return (Expr.add_int a b)
+  let open Function.Let_syntax in
+  let%bind b = Ctypes.bool in
+  let%bind x = Ctypes.int in
+  let%bind y = Ctypes.int in
+  return (Expr.cond b x y)
+;;
+
+let%expect_test "" =
+  print_endline (Compile.compile ~name:"my_function" f);
+  [%expect
+    {|
+    extern int my_function(_Bool var_0, int var_1, int var_2) {
+    _Bool var_4 = var_0;
+    int var_5 = var_1;
+    int var_6 = var_2;
+    int var_3 = var_4 ? var_5 : var_6;
+    return var_3;
+    } |}]
+;;
+
+let%expect_test "jitsy" = let f =
+   let open Function.Let_syntax in
+   let%bind b = Ctypes.bool in
+   let%bind x = Ctypes.int in
+   let%bind y = Ctypes.int in
+   return (Expr.cond b x y)
+ in
+ let%bind f = Compile.jit f in
+ print_s [%message (f true 3 5: int)];
+ print_s [%message (f false 3 5: int)];
+ [%expect {|
+   ("f true 3 5" 3)
+   ("f false 3 5" 5) |}]
+;;
+
+let%expect_test "jitsy" = let f =
+   let open Function.Let_syntax in
+   let%bind x = Ctypes.int in
+   let%bind y = Ctypes.int in
+   return (Expr.eq_int x y)
+ in
+ let%bind f = Compile.jit f in
+ print_s [%message (f 3 5: bool)];
+ print_s [%message (f 3 3: bool)];
+ [%expect {|
+   ("f 3 5" false)
+   ("f 3 3" true) |}]
+;;
+
+
+let%expect_test "" =
+  let open Ctypes in
+  print_endline (string_of_typ int);
+  [%expect "int"]
 ;;
---
+++ jitsy.mli
@@ -0,0 +1,34 @@
+open! Async
+
+module Expr : sig
+  type 'a t
+
+  val int_lit : int -> int t
+  val bool_lit : bool -> bool t
+  val add_int : int t -> int t -> int t
+  val eq_int : int t -> int t -> bool t
+  val cond : bool t -> 'a t -> 'a t -> 'a t
+  val typeof : 'a t -> 'a Ctypes.typ
+end
+
+module Type : sig
+  type 'a t
+
+  val to_string : 'a t -> string
+end
+
+module Function : sig
+  type ('a, 'r) t
+
+  module Let_syntax : sig
+    val return : 'a Expr.t -> ('a, 'a) t
+
+    module Let_syntax : sig
+      val bind : 'a Type.t -> f:('a Expr.t -> ('b, 'c) t) -> ('b, 'a -> 'c) t
+    end
+  end
+end
+
+module Compile : sig
+  val jit: ('a, ('b -> 'c)) Function.t -> ('b -> 'c) Deferred.t
+end
