76eb7569c5865920b76c8bb28890e4b83ea70d1b
Ty Overby <ty@pre-alpha.com>
2020-02-01 22:19:42 +0000

    no idea

--- examples/kissing-circles.shape.sexp
+++ examples/kissing-circles.shape.sexp
@@ -3,4 +3,5 @@
  (circle (x 60) (y 30) (r 15))
  (circle (x 30) (y 60) (r 15))
  (circle (x 60) (y 60) (r 15))
+ (circle (x 45) (y 45) (r 6.21320))
  ))
--- utilities/connected_to_svg/connected_to_svg.ml
+++ utilities/connected_to_svg/connected_to_svg.ml
@@ -12,18 +12,18 @@ let main () =
   printf
     {|<svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 88 88">
   |};
+  printf {|<path fill-rule="evenodd" d="|};
   List.iter connected ~f:(function
       | Disjoint _ -> failwith "umimplemented"
       | Joined points ->
-        printf {|<path d="|};
         (points
         |> List.hd_exn
         |> fun { Point.x; y } -> printf "M%f %f\n " x y);
         List.iter points ~f:(fun { Point.x; y } ->
             printf "L%f %f\n " x y);
-        printf " Z\" ";
-        printf
-          {|style="fill:black; stroke:none; stroke-width:0"></path>\n|});
+        printf " Z ");
+  printf
+    {|" style="fill:black; stroke:none; stroke-width:0"></path>\n|};
   printf {|</svg>|};
   Deferred.unit
 ;;
